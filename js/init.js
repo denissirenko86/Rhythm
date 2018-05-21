58488e4ef55d8ba31726614cb3209a6f424d55ae
Denis Sirenko <sirenko.denis86@gmail.com>
2018-05-21 13:14:00 +0300

    what people say

--- js/init.js
+++ js/init.js
@@ -1,4 +1,12 @@
 $( function() {
     // init tabs
     $( ".services-tab" ).tabs();
+    // init portfilio tabs
+
+    // init reviews carousel
+    $( ".reviews-carousel" ).slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000
+    });
 } );
